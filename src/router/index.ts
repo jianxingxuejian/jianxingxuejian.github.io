6220f54d76c60b0c4ea74d379eff6f182c824cb2
null <975959057@qq.com>
2022-09-06 10:55:34 +0800

    edit route mode to hash

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,12 +1,12 @@
 import type { App } from 'vue'
 import type { RouteRecordRaw } from 'vue-router'
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import modules from './modules'
 import constantRoutes from './constant-routes'
 import { createRouterGuard } from './guard'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.VITE_BASE_URL),
+  history: createWebHashHistory(import.meta.env.VITE_BASE_URL),
   routes: [...constantRoutes, ...modules] as RouteRecordRaw[]
 })
 
