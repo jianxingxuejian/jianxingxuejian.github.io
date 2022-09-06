3f73c9b85dd03cdaf393e53fe3dd9d3497e5ed34
null <975959057@qq.com>
2022-09-06 23:33:50 +0800

    update layout

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -3,13 +3,14 @@
     class="w-screen h-screen px-5% grid xl:grid-cols-[22fr_66fr] 2xl:grid-cols-[18fr_48fr_18fr] gap-x-6"
   >
     <div class="h-0 order-0 grid-col-start-1 grid-col-end-1"></div>
-    <layout-main class="order-1 xl:order-2" />
+    <layout-main class="order-1 xl:order-2 grid-row-span-4" />
     <aside class="order-2 xl:order-1 grid-col-start-1 grid-col-end-1">
-      <layout-author class="" />
-      <layout-classify class="sticky top-20px" />
+      <layout-author />
+      <layout-classify />
     </aside>
     <aside class="order-3">
-      <layout-about class="sticky top-20px" />
+      <layout-about />
+      <layout-about />
     </aside>
   </div>
 </template>
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="w-full h-3000px">
+  <div class="w-full h-3000px bg-yellow-2">
     <span>hello world!</span>
   </div>
 </template>
