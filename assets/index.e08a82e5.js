00b3cfe26adcfe0f35766eac06828821caead7ca
jianxingxuejian <jianxingxuejian@users.noreply.github.com>
2022-09-07 16:47:02 +0000

    deploy: d4ca0bdbb8fc2aa1b7abe718f2fb90f8708ba1f1

---
+++ assets/index.e08a82e5.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./plugin-vueexport-helper.2444895f.js";import{o,p as s,q as t}from"./index.681f0053.js";const c={},n={class:"w-full h-3000px bg-yellow-2"},r=t("span",null,"hello world!",-1),_=[r];function l(a,d){return o(),s("div",n,_)}const p=e(c,[["render",l]]);export{p as default};
