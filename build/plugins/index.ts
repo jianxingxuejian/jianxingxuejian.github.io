5b1e5ac705229fdafdcf0274946388960b6651aa
null <975959057@qq.com>
2022-09-14 20:59:00 +0800

    add compress plugin, update dependencies

--- build/plugins/index.ts
+++ build/plugins/index.ts
@@ -1,4 +1,5 @@
 import type { PluginOption } from 'vite'
+import compress from './compress'
 import html from './html'
 import https from './https'
 import unocss from './unocss'
@@ -9,6 +10,6 @@ import vue from './vue'
  * 安装vite插件
  */
 export function setupVitePlugins(env: ImportMetaEnv): PluginOption[] {
-  const plugins = [html(env), https, unocss, ...unplugin, vue]
+  const plugins = [compress, html(env), https, unocss, ...unplugin, vue]
   return plugins
 }
