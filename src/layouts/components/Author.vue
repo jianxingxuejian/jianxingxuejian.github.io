d299b4e50550b6de6da0f252eb568c80a899ea49
null <975959057@qq.com>
2022-09-27 00:04:46 +0800

    update

--- src/layouts/components/Author.vue
+++ src/layouts/components/Author.vue
@@ -3,10 +3,12 @@
     <div class="flex-col items-center">
       <img
         src="https://q1.qlogo.cn/g?b=qq&s=640&nk=975959057"
-        class="b-rd-50% w-50% max-w-25vw select-none"
+        class="b-rd-50% w-45% max-w-22.5vw select-none"
       />
-      <span class="text-6">null</span>
-      <span class="text-4">xxxxxxxxxxxxxx</span>
+      <span class="text-7">null</span>
+      <span class="text-4 text-center"
+        >纵然被放逐在地球的渺小一隅，人们仍在仰望星空。</span
+      >
     </div>
   </n-card>
 </template>
